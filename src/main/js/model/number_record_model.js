6b713cff52f7b14d5c571812c9b0c79c21f29643
Warren Galyen <wgalyen@hotmail.com>
2019-09-22 12:37:33 -0700

    Fix bug in updating graph monitor

--- src/main/js/model/number_record_model.js
+++ src/main/js/model/number_record_model.js
@@ -1,33 +1,37 @@
 const NumberModel = require('./number_model');
 
 class NumberRecordModel extends NumberModel {
-    constructor(opt_recordCount) {
-        super();
+	constructor(opt_recordCount) {
+		super();
 
-        this.recValues_ = [];
-        this.recCount_ = (opt_recordCount !== undefined) ?
-            opt_recordCount :
-            NumberRecordModel.DEFAULT_RECORD_COUNT;
-        for (let i = 0; i < this.recCount_; i++) {
-            this.recValues_.push(0);
-        }
-    }
+		this.recValues_ = [];
+		this.recCount_ = (opt_recordCount !== undefined) ?
+			opt_recordCount :
+			NumberRecordModel.DEFAULT_RECORD_COUNT;
+		for (let i = 0; i < this.recCount_; i++) {
+			this.recValues_.push(0);
+		}
+	}
 
-    getRecords() {
-        return this.recValues_;
-    }
+	getRecords() {
+		return this.recValues_;
+	}
 
-    setValue(value) {
-        if (super.setValue(value)) {
-            this.recValues_.push(this.value_);
+	setValue(value) {
+		super.setValue(value);
 
-            if (this.recValues_.length > this.recCount_) {
-                this.recValues_.splice(0, this.recValues_.length - this.recCount_);
-            }
-        }
-    }
+		this.recValues_.push(this.value_);
+		if (this.recValues_.length > this.recCount_) {
+			this.recValues_.splice(0, this.recValues_.length - this.recCount_);
+		}
+
+		this.getEmitter().notifyObservers(
+			NumberRecordModel.EVENT_RECORD_CHANGE
+		);
+	}
 }
 
 NumberRecordModel.DEFAULT_RECORD_COUNT = 200;
+NumberRecordModel.EVENT_RECORD_CHANGE = 'recordchange';
 
 module.exports = NumberRecordModel;
--- src/main/js/view/monitor/graph_monitor.js
+++ src/main/js/view/monitor/graph_monitor.js
@@ -1,85 +1,96 @@
 const MaxNumberConstraint = require('../../constraint/max_number_constraint');
 const MinNumberConstraint = require('../../constraint/min_number_constraint');
-const ClassName           = require('../../misc/class_name');
-const Monitor             = require('./monitor');
+const ClassName = require('../../misc/class_name');
+const NumberRecordModel = require('../../model/number_record_model');
+const Monitor = require('./monitor');
 
 class GraphMonitor extends Monitor {
-    constructor(property) {
-        super(property);
+	constructor(property) {
+		super(property);
 
-        const elem = this.getElement();
-        elem.classList.add(
-            ClassName.get(GraphMonitor.BLOCK_CLASS)
-        );
+		const prop = this.getProperty();
+		prop.getModel().getEmitter().on(
+			NumberRecordModel.EVENT_RECORD_CHANGE,
+			this.onModelRecordChange_.bind(this)
+		);
 
-        const canvasElem = document.createElement('canvas');
-        canvasElem.classList.add(
-            ClassName.get(GraphMonitor.BLOCK_CLASS, 'canvas')
-        );
-        elem.appendChild(canvasElem);
-        this.canvasElem_ = canvasElem;
+		const elem = this.getElement();
+		elem.classList.add(
+			ClassName.get(GraphMonitor.BLOCK_CLASS)
+		);
 
-        this.color_ = '';
+		const canvasElem = document.createElement('canvas');
+		canvasElem.classList.add(
+			ClassName.get(GraphMonitor.BLOCK_CLASS, 'canvas')
+		);
+		elem.appendChild(canvasElem);
+		this.canvasElem_ = canvasElem;
 
-        this.applyModel_();
-    }
+		this.color_ = '';
 
-    getMaxValue_() {
-        const maxConstraint = this.getProperty().getModel().findConstraintByClass(MaxNumberConstraint);
-        return (maxConstraint !== null) ?
-            maxConstraint.getMaxValue() :
-            +1.0;
-    }
+		this.applyModel_();
+	}
 
-    getMinValue_() {
-        const minConstraint = this.getProperty().getModel().findConstraintByClass(MinNumberConstraint);
-        return (minConstraint !== null) ?
-            minConstraint.getMinValue() :
-            -1.0;
-    }
+	getMaxValue_() {
+		const maxConstraint = this.getProperty().getModel().findConstraintByClass(MaxNumberConstraint);
+		return (maxConstraint !== null) ?
+			maxConstraint.getMaxValue() :
+			+1.0;
+	}
 
-    applyModel_() {
-        super.applyModel_();
+	getMinValue_() {
+		const minConstraint = this.getProperty().getModel().findConstraintByClass(MinNumberConstraint);
+		return (minConstraint !== null) ?
+			minConstraint.getMinValue() :
+			-1.0;
+	}
 
-        this.applyCanvasSizeIfNeeded_();
+	applyModel_() {
+		super.applyModel_();
 
-        const canvas = this.canvasElem_;
-        const w = canvas.width;
-        const h = canvas.height;
-        const g = canvas.getContext('2d');
-        g.clearRect(0, 0, canvas.width, canvas.height);
+		this.applyCanvasSizeIfNeeded_();
 
-        if (this.color_ === '') {
-            this.color_ = window.getComputedStyle(canvas).color;
-        }
-        g.strokeStyle = this.color_;
+		const canvas = this.canvasElem_;
+		const w = canvas.width;
+		const h = canvas.height;
+		const g = canvas.getContext('2d');
+		g.clearRect(0, 0, canvas.width, canvas.height);
 
-        const minValue = this.getMinValue_();
-        const maxValue = this.getMaxValue_();
+		if (this.color_ === '') {
+			this.color_ = window.getComputedStyle(canvas).color;
+		}
+		g.strokeStyle = this.color_;
 
-        const model = this.getProperty().getModel();
-        const values = model.getRecords();
-        g.beginPath();
-        const y0 = h * ((values[0] - minValue) / (maxValue - minValue));
-        g.moveTo(0, h - y0);
-        for (let x = 1; x < w; x++) {
-            const index = Math.floor(values.length * x / w);
-            const v = values[index];
-            const y = h * ((v - minValue) / (maxValue - minValue));
-            g.lineTo(x, h - y);
-        }
-        g.stroke();
-    }
+		const minValue = this.getMinValue_();
+		const maxValue = this.getMaxValue_();
 
-    applyCanvasSizeIfNeeded_() {
-        const canvas = this.canvasElem_;
-        if (canvas.width !== canvas.clientWidth) {
-            canvas.width = canvas.clientWidth;
-        }
-        if (canvas.height !== canvas.clientHeight) {
-            canvas.height = canvas.clientHeight;
-        }
-    }
+		const model = this.getProperty().getModel();
+		const values = model.getRecords();
+		g.beginPath();
+		const y0 = h * ((values[0] - minValue) / (maxValue - minValue));
+		g.moveTo(0, h - y0);
+		for (let x = 1; x < w; x++) {
+			const index = Math.floor(values.length * x / w);
+			const v = values[index];
+			const y = h * ((v - minValue) / (maxValue - minValue));
+			g.lineTo(x, h - y);
+		}
+		g.stroke();
+	}
+
+	applyCanvasSizeIfNeeded_() {
+		const canvas = this.canvasElem_;
+		if (canvas.width !== canvas.clientWidth) {
+			canvas.width = canvas.clientWidth;
+		}
+		if (canvas.height !== canvas.clientHeight) {
+			canvas.height = canvas.clientHeight;
+		}
+	}
+
+	onModelRecordChange_() {
+		this.applyModel_();
+	}
 }
 
 GraphMonitor.BLOCK_CLASS = 'grm';
