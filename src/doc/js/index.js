315b78c548cb6cfdf883316b8556e005b9b1fd2f
Warren Galyen <wgalyen@hotmail.com>
2019-09-22 19:23:46 -0700

    Update sketch

--- src/doc/js/index.js
+++ src/doc/js/index.js
@@ -5,11 +5,11 @@ var sketchParams = sketch.getParameters();
 var nigiri = sketch.getNigiri();
 var neta = nigiri.getNeta();
 
-var aero = Aerodial({
+var pane = Aerodial({
 	container: document.getElementById('tp')
 });
 
-var folder1 = aero.addFolder('Appearance');
+var folder1 = pane.addFolder('Appearance');
 folder1.add(sketchParams, 'neta', {
 	list: [
 		'hotate',
@@ -18,27 +18,32 @@ folder1.add(sketchParams, 'neta', {
 	]
 });
 
-var folder2 = aero.addFolder('Physics');
+var folder2 = pane.addFolder('Physics');
 folder2.add(sketchParams, 'gravity', {
 	min: 0.1,
 	max: 1.0
 });
 folder2.add(sketchParams, 'airResistance', {
-	min: 0.0,
+	min: 0.01,
 	max: 0.1
 });
 folder2.add(sketchParams, 'restitution', {
 	min: 0.0,
-	max: 1.0
+	max: 0.9
 });
 folder2.addSeparator();
 folder2.monitor(neta, 'z', {
-	label: 'netaZ',
+	label: 'netaAlt',
+	count: 140,
 	graph: true,
 	min: 0,
 	max: 100
 });
 folder2.addSeparator();
-folder2.addButton('Pop').on('click', function() {
+folder2.addButton('Pop!').on('click', function() {
 	nigiri.pop();
 });
+
+setInterval(function() {
+	nigiri.pop();
+}, 4000);
