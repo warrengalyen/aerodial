e57076cc81760a5a0aa3a90d3bbb5e91cb8b3326
Warren Galyen <wgalyen@hotmail.com>
2019-09-24 07:50:32 -0700

    Update demo code

--- src/doc/js/common.js
+++ src/doc/js/common.js
@@ -1,12 +1,9 @@
-var dummy = {
-	'TODO': 0
-};
 var aero = Aerodial({
 	foldable: false,
 	container: document.getElementsByClassName('hero_aeroContainer').item(0)
 });
 
-aero.add(dummy, 'TODO', {
+aero.add({slider: 0}, 'slider', {
 	min: 0,
 	max: 100,
 });
