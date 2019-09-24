6cf219d1c75dbcae547b411b6a13912627c0b010
Warren Galyen <wgalyen@hotmail.com>
2019-09-24 07:48:25 -0700

    Fix layout issue with 🍣

--- src/doc/js/common.js
+++ src/doc/js/common.js
@@ -1,12 +1,12 @@
 var dummy = {
 	'TODO': 0
 };
-var pane = Aerodial({
+var aero = Aerodial({
 	foldable: false,
-	container: document.getElementsByClassName('hero_paneContainer').item(0)
+	container: document.getElementsByClassName('hero_aeroContainer').item(0)
 });
 
-pane.add(dummy, 'TODO', {
+aero.add(dummy, 'TODO', {
 	min: 0,
 	max: 100,
 });
