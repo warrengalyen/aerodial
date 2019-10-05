7f1322bd79594a59e521036d3bfdad34da6e1c82
Warren Galyen <wgalyen@hotmail.com>
2019-10-05 08:29:22 -0700

    Update node modules & fix build path

--- src/main/js/aerodial.js
+++ src/main/js/aerodial.js
@@ -1,9 +1,11 @@
+import FolderViewInterface from './interface/folder_view_interface';
 import RootViewInterface from './interface/root_view_interface';
+import ComponentUtil       from './misc/component_util';
 import RootView          from './view/root_view';
 
-const Aerodial = (opt_options) => {
+// Assign here to avoid circular reference
+FolderViewInterface.componentUtil__ = ComponentUtil;
+export default function Aerodial(opt_options) {
 	const rootView = new RootView();
 	return new RootViewInterface(rootView, opt_options);
 };
-
-export default Aerodial;
